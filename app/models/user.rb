f4e9525a4f007515416cdd6353054d02ea238458
cookethan <ethancook48@gmail.com>
2020-02-11 20:22:29 -0600

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+
+  has_many :tasks
+
+  def due_today
+    self.tasks.select{ |t| t.due_date.to_date == DateTime.now.to_date }
+  end
+end
---
+++ spec/features/tasks_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+
+feature "tasks/index" do
+  let(:user) { create(:user) }
+  let(:task) { create(:homework) }
+  scenario "renders a list of tasks" do
+    user = create(:user)
+    sign_in(user)
+
+    create(:homework, user: user)
+    create(:email, user: user)
+
+    visit tasks_path
+
+    expect(page).to have_content('complete homework')
+    expect(page).to have_content("reply to Zack's email")
+  end
+end
+
+
+feature 'New Task' do
+  let(:user) { create(:user) }
+  let(:task) { create(:homework) }
+  scenario 'user adds a new task' do
+    user = create(:user)
+    sign_in(user)
+    visit tasks_path
+
+    expect{
+      click_link 'New Task'
+      fill_in 'Name', with: 'Learn Rspec'
+      fill_in 'Priority', with: 1
+      fill_in 'Due date', with: DateTime.now
+      select(user.email, from: 'task_user_id')
+      click_button('Create Task')
+    }.to change(Task, :count).by(1)
+
+    expect(current_path).to eq(task_path(Task.last.id))
+    expect(page).to have_content('Learn Rspec')
+
+  end
+end
+
+feature 'Edit Task' do
+  let(:user) { create(:user) }
+  let(:task) { create(:homework) }
+
+  scenario 'User edits task' do
+    sign_in(user)
+    visit task_path(task)
+    expect(page).to have_content('complete homework')
+
+    click_link("Edit")
+
+    fill_in 'Name', with: 'Master Rspec!!!!!'
+    fill_in "Priority", with: 1
+    fill_in "Due date", with: DateTime.now
+    select(task.user.email, from: 'task_user_id')
+
+    click_button('Update Task')
+
+    expect(current_path).to eq(task_path(task.id))
+
+    expect(page).to have_content('Master Rspec!!!!!')
+  end
+end
